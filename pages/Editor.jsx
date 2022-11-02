dee2f905761eff5b16a1f80e4b9463d171814850
eshromanch <romanch.haq868.gmail.com>
2022-11-02 18:30:05 +0600

    editor2

---
+++ pages/Editor.jsx
@@ -0,0 +1,63 @@
+import dynamic from 'next/dynamic';
+
+const QuillNoSSRWrapper = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
+
+const modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+/*
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'video',
+];
+
+export default function Editor() {
+  return (
+<div className='h-full w-full flex justify-center items-center my-12'>
+<QuillNoSSRWrapper className='text-black w-[50vw]'
+      modules={modules}
+      formats={formats}
+      theme="snow"
+      
+      onChange={(content) => {
+        // var htmlToRtf = require('html-to-rtf');
+        // console.log('CONTETN: ', content);
+      }}
+    />
+</div>
+  );
+}
