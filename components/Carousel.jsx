7c534d8d88c4aa5da6a394520d19c89e8c061654
eshromanch <romanch.haq868.gmail.com>
2022-11-06 16:35:02 +0600

    carousel fixed

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -24,11 +24,11 @@ useEffect(()=>{
     return (
 
       <div className={`hero relative h-[75vh] w-full overflow-hidden `} > 
-       <img className='absolute -z-1 object-fit w-full h-full ' src={`${data?.[0]['image']}`} alt="" /> 
+       <img className='absolute -z-1 object-fit w-full h-full lg:object-cover' src={`${data?.[0]['image']}`} alt="" /> 
 
 
 
-       <div className="grid-rows-2  h-full w-full hero-content  text-neutral-content sm:flex sm:flex-col">
+       <div className="grid-rows-2  h-full w-full hero-content  text-neutral-content sm:flex sm:flex-col sm:ml-[80%]">
          <div className="w-full">
 
        <Link
@@ -37,19 +37,19 @@ useEffect(()=>{
          legacyBehavior><h1 className="text-5xl text-white  group transition duration-300 sm:text-xl">{data?.[0]['title_bn']}<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-primary"></span></h1></Link>
          
          </div>
-         <div className="flex flex-col  justify-center items-center w-full gap-4 p-10">
+         <div className="flex flex-col translate-x-1/2 w-full gap-4 lg:translate-x-0 lg:justify-center ">
      {
-       data?.slice(1).map(item =>{
+       data?.slice(1,5).map(item =>{
         
      if (item.status==="1") {
        return item.post_to_cat.map(cat=>{
          return (
-           <div key={item.id} className='flex flex-rows justify-start items-center h-full w-full gap-4  ' >
-       <Image   className='w-32 h-32 sm:w-14 sm:h-14 object-cover' src={`${item.image}`} width="100" height="100" quality={75}></Image>
+           <div key={item.id} className='flex justify-center items-center h-full  gap-4  ' >
+       <Image   className='w-28 h-28 sm:w-14 sm:h-14 object-cover' src={`${item.image}`} width="100" height="100" quality={75}></Image>
            {/* <img className='w-32 h-32 sm:w-14 sm:h-14 object-cover' src={`${item.image}`} alt="" /> */}
            <Link
              href={`categories/${cat.category_id}/articles/${cat.post_id}`}
-             legacyBehavior><h1 className="text-2xl text-white font-semibold group transition duration-300 sm:text-xs sm:font-normal">{item.title_bn}<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-primary"></span></h1></Link>
+             legacyBehavior><h1 className="text-2xl text-white w-full font-semibold group transition duration-300 sm:text-xs sm:font-normal">{item.title_bn}<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-primary"></span></h1></Link>
          </div>
          ); 
        }); 
