9ecc2b6ed48e5e6877f04e787cb2ec0bd3dfe32f
eshromanch <romanch.haq868.gmail.com>
2022-11-14 18:46:37 +0600

    image gallery issue2

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,23 @@
-import Layouts from '../components/Layouts'
-import '../styles/globals.css'
-import 'react-quill/dist/quill.snow.css'
-import { ThemeProvider } from 'next-themes'
-import Footer from '../components/Footer';
-import Nav from '../components/Nav';
-import React, {useEffect} from "react";
+import Layouts from "../components/Layouts";
+import "../styles/globals.css";
+import "react-quill/dist/quill.snow.css";
+import { ThemeProvider } from "next-themes";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import React, { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     document.getElementById("__next").className = "dark:bg-slate-800 relative";
   }, []);
-  return  <ThemeProvider attribute="class" enableSystem={true}>
-                <Nav/>
-   <Layouts>
-    <Component {...pageProps} />
-  </Layouts>
-  <Footer/> 
-  </ThemeProvider>
-
+  return (
+    <ThemeProvider attribute="class" enableSystem={true}>
+      <Nav />
+      <Layouts>
+        <Component {...pageProps} />
+      </Layouts>
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
