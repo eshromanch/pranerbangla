8b73188c6d6b2bc43fd8dd57a5096f84c1af31f7
eshromanch <romanch.haq868.gmail.com>
2022-11-15 17:43:51 +0600

    image gallery issue10

--- components/HomepageComponents/Gridimages.jsx
+++ components/HomepageComponents/Gridimages.jsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 import Icons from "../svgs/GalleryIcon"
+
 function Gridimages(props) {
   return (
     <div className='h-full overflow-hidden flex'>
 
-<figure className='group h-full w-full relative traansform transition cursor-pointer  hover:scale-110 group-hover:shadow hover:shadow-inner-lg '><Icons className="absolute h-12 m-2 group-hover:m-10  duration-300 md:h-5"/> <img  className='hedarBal object-fill  ' src={props.img} alt="" />
+<figure className='group h-full w-full relative traansform transition cursor-pointer  hover:scale-110 group-hover:shadow hover:shadow-inner-lg '><Icons className="absolute h-12 m-2 group-hover:m-10  duration-300 md:h-5"/> 
+<Image className='w-full h-full object-fill'  src={props.img}  width='500' height='500'></Image>
+{/* <img  className='h-ful object-fill  ' src={props.img} alt="" /> */}
       <div className=' absolute bottom-8 w-full  opacity-0 group-hover:opacity-100 group-hover:mx-10    duration-300'><h1 className='text-white text-2xl'>{props.title}</h1>
       
       </div>
