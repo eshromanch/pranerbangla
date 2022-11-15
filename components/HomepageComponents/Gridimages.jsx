8b171f3ce056a16a8ff327f072df409a6460278e
eshromanch <romanch.haq868.gmail.com>
2022-11-15 18:14:48 +0600

    image gallery issue12

--- components/HomepageComponents/Gridimages.jsx
+++ components/HomepageComponents/Gridimages.jsx
@@ -8,6 +8,7 @@ function Gridimages(props) {
 
 <figure className='group h-full w-full relative traansform transition cursor-pointer  hover:scale-110 group-hover:shadow hover:shadow-inner-lg '><Icons className="absolute h-12 m-2 group-hover:m-10 md:group-hover:m-5  duration-300 md:h-5"/> 
 <Image className='w-full h-full object-fill'  src={props.img}  width='500' height='500'></Image>
+
 {/* <img  className='h-ful object-fill  ' src={props.img} alt="" /> */}
       <div className=' absolute bottom-8 w-full  opacity-0 group-hover:opacity-100 group-hover:mx-10    duration-300 md:hidden'><h1 className='text-white text-2xl'>{props.title}</h1>
       
