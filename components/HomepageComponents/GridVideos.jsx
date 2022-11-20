b137c87862bc3fdffa6eead5f99bf7f1699fb806
eshromanch <romanch.haq868.gmail.com>
2022-11-20 18:54:47 +0600

    video section

--- components/HomepageComponents/GridVideos.jsx
+++ components/HomepageComponents/GridVideos.jsx
@@ -23,11 +23,11 @@ useEffect(()=>{
 
   return (
     <div className='w-full bg-[#202426] p-10 mb-4'>
-    <div className='mx-80 flex justify-between items-center lg:mx-24'><b><h1 className='flex  justify-between items-center text-lg  text-white mb-4 dark:text-white'><Circle className=" h-3  pr-2 mb-1"/>ভিডিও   </h1></b>
+    <div className='mx-80 flex justify-between items-center lg:mx-24 md:mx-5'><b><h1 className='flex  justify-between items-center text-lg  text-white mb-4 dark:text-white'><Circle className=" h-3  pr-2 mb-1"/>ভিডিও   </h1></b>
     <b><Link legacyBehavior href={"Allvideos"}><a className='btn btn-ghost text-base text-white mb-4 dark:text-white' href="#">
     <h4 className='flex '><Plus className="h-5 pr-2 mt-0"/> আরো দেখুন </h4></a></Link></b>
     </div>
-    <div className='mx-80 grid grid-cols-4 gap-10 md:mx-32'>
+    <div className='mx-80 grid grid-cols-4 gap-10 md:mx-5 md:gap-2'>
       <div className='col-span-2'>
         {
           allVideo?.slice(0,1).map(items =>{
