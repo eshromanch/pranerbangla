a28333db7f4063e4a8614a63bdd984aa2b357bd9
eshromanch <romanch.haq868.gmail.com>
2022-11-27 16:54:08 +0600

    editorial design update

--- components/HomepageComponents/Edotorial.jsx
+++ components/HomepageComponents/Edotorial.jsx
@@ -17,7 +17,7 @@ function Edotorial(props) {
 
     return (
         <div className='flex flex-col '><b><h1 className='mb-4 flex text-lg text-black dark:text-white'><Circle className=" h-3 pr-2 mt-1"/>সম্পাদকীয়</h1></b>
-        <div className='flex flex-col'>
+        <div className='flex flex-col  card rounded-md   shadow-lg'>
           {/* <div className='h-40 w-40 bg-gray-500'></div> */}
           <div className='h-[18rem] relative flex flex-col items-center  '>
             <img className='h-[12.5rem] w-full object-cover' src={data?.['image']} alt="" />
@@ -26,7 +26,7 @@ function Edotorial(props) {
         
      </div>
             </div>
-          <p className='my-8 text-base text-black dark:text-white' dangerouslySetInnerHTML={{ __html: data?.content_bn }}></p>
+          <p className='my-8 text-base text-black dark:text-white px-4' dangerouslySetInnerHTML={{ __html: data?.content_bn }}></p>
           <img src={`https://pranerbangla.com.bd/${data?.signature}`} alt=""  />
         </div>
         </div>
